a31de8424cc9df81c04c9c3a3fa880d326eab36d
michaelzheng67 <michaelzheng67@yahoo.ca>
2023-11-13 19:42:27 -0500

    polygon websocket connection

--- backend/go.mod
+++ backend/go.mod
@@ -2,18 +2,17 @@ module realtime_stock_chat
 
 go 1.21.3
 
-require github.com/polygon-io/client-go v1.15.0
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/polygon-io/client-go v1.15.0
+	github.com/sirupsen/logrus v1.9.3
+	golang.org/x/net v0.17.0
+)
 
 require (
-	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
-	github.com/go-playground/form/v4 v4.2.1 // indirect
-	github.com/go-playground/locales v0.14.1 // indirect
-	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.15.5 // indirect
-	github.com/go-resty/resty/v2 v2.9.1 // indirect
-	github.com/leodido/go-urn v1.2.4 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/exp v0.0.0-20220414153411-bcd21879b8fd // indirect
 	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	gopkg.in/tomb.v2 v2.0.0-20161208151619-d5d1b5820637 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,22 +1,169 @@
 package main
 
 import (
-    "log"
-    "time"
-    "context"
-    polygon "github.com/polygon-io/client-go/rest"
-	"github.com/polygon-io/client-go/rest/models"
+	"os"
+	"os/signal"
+    "encoding/json"
+
+	polygonws "github.com/polygon-io/client-go/websocket"
+	"github.com/polygon-io/client-go/websocket/models"
+	"github.com/sirupsen/logrus"
+
+    "golang.org/x/net/websocket"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/joho/godotenv"
 )
 
-func main() {
-    c := polygon.New("qblnPkGxwqopCBRFvlJDiduK5iZhxJDv")
-    params := models.GetTickerDetailsParams{
-        Ticker: "AAPL",
-    }.WithDate(models.Date(time.Date(2021, 7, 22, 0, 0, 0, 0, time.Local)))
-    
-    res, err := c.GetTickerDetails(context.Background(), params)
-    if err != nil {
-        log.Fatal(err)
+
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("new incoming connection from client:", ws.RemoteAddr())
+
+	s.conns[ws] = true
+
+	s.readLoop(ws)
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error:", err)
+			continue
+		}
+		msg := buf[:n]
+		
+		s.broadcast(msg)
+	}
+}
+
+func (s *Server) broadcast(b []byte) {
+	for ws := range s.conns {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				fmt.Println("write error: ", err)
+			}
+		}(ws)
+	}
+} 
+
+func sendData(url string, origin string) {
+
+	// load env variables
+	err := godotenv.Load("variables.env")
+	if err != nil {
+        fmt.Println("Error loading .env file")
     }
-    log.Print(res) // do something with the result
+
+
+	// create websocket client
+	ws, err := websocket.Dial(url, "", origin)
+    if err != nil {
+        fmt.Println("Error connecting to WebSocket server:", err)
+        os.Exit(1)
+    }
+    defer ws.Close()
+
+
+    log := logrus.New()
+	log.SetLevel(logrus.DebugLevel)
+	log.SetFormatter(&logrus.JSONFormatter{})
+	c, err := polygonws.New(polygonws.Config{
+		APIKey: os.Getenv("POLYGON_API_KEY"),
+		Feed:   polygonws.Delayed,
+		Market: polygonws.Stocks,
+		Log:    log,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	// aggregates
+	_ = c.Subscribe(polygonws.StocksSecAggs, "AAPL", "TSLA", "AMZN", "GOOG", "META", "BABA", "MSFT")
+
+	if err := c.Connect(); err != nil {
+		log.Error(err)
+		return
+	}
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+
+	for {
+		select {
+		case <-sigint:
+			return
+		case <-c.Error():
+			return
+		case out, more := <-c.Output():
+			if !more {
+				return
+			}
+			switch out.(type) {
+
+			case models.EquityAgg:
+				log.WithFields(logrus.Fields{"aggregate": out}).Info()
+
+				jsonData, err := json.Marshal(out)
+				if err != nil {
+					fmt.Println("Error marshalling JSON:", err)
+					return
+				}
+
+				if err := websocket.Message.Send(ws, jsonData); err != nil {
+					fmt.Println("Error sending message:", err)
+					return
+				}
+
+                
+
+			case models.EquityTrade:
+				log.WithFields(logrus.Fields{"trade": out}).Info()
+			case models.EquityQuote:
+				log.WithFields(logrus.Fields{"quote": out}).Info()
+			}
+		}
+	}
+}
+
+func main() {
+
+	// :8000 - stocks
+	// :8080 - trader chat
+
+    stock_server := NewServer()
+	go func() {
+		http.Handle("/stock-ws", websocket.Handler(stock_server.handleWS))
+		if err := http.ListenAndServe(":8000", nil); err != nil {
+			fmt.Printf("Failed to start stock server: %v", err)
+		}
+	}()
+
+	trader_server := NewServer()
+	go func() {
+		http.Handle("/trader-ws", websocket.Handler(trader_server.handleWS))
+		if err := http.ListenAndServe(":9000", nil); err != nil {
+			fmt.Printf("Failed to start trader server: %v", err)
+		}
+	}()
+	
+    sendData("ws://localhost:8000/stock-ws", "http://localhost/");
+
 }
